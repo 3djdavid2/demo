6ac56b883b949442d30f56661e222597f41f069f
David Vivanco <desarrollowebdavid@gmail.com>
2023-07-16 11:38:36 -0400

    more tha one router-outlet ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,21 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 
 export const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
+  {
+    path: 'welcome', component: WelcomeComponent,
+    children: [
+      {
+        path:'about',
+        loadComponent:()=> import('../app/about/about.component').then(c=>c.AboutComponent)
+        
+      }
+    ]
+  },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   {
     path: 'products',
     loadChildren: () => import('./products/product.routes').then(r => r.PRODUCT_ROUTES)
   },
-  { 
-    path: 'about', 
-    loadComponent: () => import('./about/about.component').then(c => c.AboutComponent)
-  },
+
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
 ];
--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule,RouterOutlet } from '@angular/router'
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule,RouterOutlet],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
